c5f25447b829f137e05b01a407271f039e281b9f
Austin <austinc726@gmail.com>
2017-04-11 22:21:17 -0400

    Update DiscordTestBot.java

--- DiscordTestBot.java
+++ DiscordTestBot.java
@@ -14,40 +14,25 @@ import javax.security.auth.login.LoginException;
 import java.util.List;
 import java.util.Random;
 
-public class DiscordTestBot extends ListenerAdapter
-{
-    /**
-     * This is the method where the program starts.
-     */
-    public static void main(String[] args)
-    {
-        //We construct a builder for a BOT account. If we wanted to use a CLIENT account
-        // we would use AccountType.CLIENT
-        try
-        {
+public class DiscordTestBot extends ListenerAdapter {
+    
+    public static void main(String[] args) {
+        
+        try {
             JDA jda = new JDABuilder(AccountType.BOT)
-                    .setToken("MjczMzA3MjYxNzE4MzY0MTYx.C2k97w.oYed3TDce6vWIrUFxoUCME9ZGXM")           //The token of the account that is logging in.
-                    .addListener(new DiscordTestBot())  //An instance of a class that will handle events.
-                    .buildBlocking();  //There are 2 ways to login, blocking vs async. Blocking guarantees that JDA will be completely loaded.
+                    .setToken("bot token goes here")           
+                    .addListener(new DiscordTestBot())  
+                    .buildBlocking(); 
         }
-        catch (LoginException e)
-        {
-            //If anything goes wrong in terms of authentication, this is the exception that will represent it
+        catch (LoginException e) {    
+            
             e.printStackTrace();
         }
-        catch (InterruptedException e)
-        {
-            //Due to the fact that buildBlocking is a blocking method, one which waits until JDA is fully loaded,
-            // the waiting can be interrupted. This is the exception that would fire in that situation.
-            //As a note: in this extremely simplified example this will never occur. In fact, this will never occur unless
-            // you use buildBlocking in a thread that has the possibility of being interrupted (async thread usage and interrupts)
+        catch (InterruptedException e) {
+            
             e.printStackTrace();
         }
-        catch (RateLimitedException e)
-        {
-            //The login process is one which can be ratelimited. If you attempt to login in multiple times, in rapid succession
-            // (multiple times a second), you would hit the ratelimit, and would see this exception.
-            //As a note: It is highly unlikely that you will ever see the exception here due to how infrequent login is.
+        catch (RateLimitedException e) {
             e.printStackTrace();
         }
     }
